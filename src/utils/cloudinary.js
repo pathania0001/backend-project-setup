39a41513db2d7f93d7eaed003b43fcbaaa1e0780
pathania0001 <83996978+pathania0001@users.noreply.github.com>
2024-03-05 20:07:28 +0530

    all till registration

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -12,7 +12,7 @@ cloudinary.config({
 
 
 const uploadOnCloudinary = async (localFilePath)=>
-{    console.log("hii")
+{    
     try {
         if (!localFilePath) return null
         //upload the file on cloudinary
@@ -28,5 +28,5 @@ const uploadOnCloudinary = async (localFilePath)=>
         return null;
     }
 }
-uploadOnCloudinary("")
 
+export  {uploadOnCloudinary}
